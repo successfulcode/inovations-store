1ff76dc8ec0a8307c22f6c3526b1a459ec09db98
Sergej Mickevic <Sergej@successfulcode.lan>
2023-05-18 17:25:11 +0300

    Added a user controller swagger description

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { User } from './users.model';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
 
