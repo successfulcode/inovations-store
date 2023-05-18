0facb60747f5d66030d0134604669256034d1ecb
Sergej Mickevic <Sergej@successfulcode.lan>
2023-05-18 10:52:30 +0300

    Added space

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = 3000 || process.env.PORT;
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
 }
 
