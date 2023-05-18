0c2b2cf2ce8f73158df140be09bf4aa775e08f51
Sergej Mickevic <Sergej@successfulcode.lan>
2023-05-18 17:17:06 +0300

    Added descriptions for a swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'test@test.com', description: 'The user email' })
   readonly email: string;
+  
+  @ApiProperty({ example: 'test12345', description: 'The user password' })
   readonly password: string;
 }
