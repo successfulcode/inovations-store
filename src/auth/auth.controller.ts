f2bd75bda008bfb00bee4b63726d2ff0b1e5f2c4
Sergej Mickevic <Sergej@successfulcode.lan>
2023-05-19 18:54:37 +0300

    Created auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
+@ApiTags('Authorization')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    this.authService.login(userDto);
+  }
+
+  @Post('/signup')
+  signUp(@Body() userDto: CreateUserDto) {
+    return this.authService.signUp(userDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs'
+import { User } from 'src/users/models/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async login(userDto: CreateUserDto) {
+
+  }
+
+  async generateToken(user: User) {
+    const payload = { email: user.email, id: user.id, roles: user.roles };
+
+    return {
+      token: this.jwtService.sign(payload)
+    }
+  }
+
+  async signUp(userDto: CreateUserDto) {
+    const candidate = await this.usersService.getUserByEmail(userDto.email);
+
+    if (candidate) {
+      throw new HttpException('The user already exist', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
+
+    const user = await this.usersService.createUser({ ...userDto, password: hashPassword });
+
+    return this.generateToken(user)
+  }
+}
